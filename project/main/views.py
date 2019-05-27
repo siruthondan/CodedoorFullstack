5cef39c684a50fe7ac69a5e552b0e7af2aa98aac
Siruthondan <siruthondan@gmail.com>
2019-05-27 21:47:37 +0200

    Completed the Challenge

--- project/main/views.py
+++ project/main/views.py
@@ -4,6 +4,7 @@
 from flask import render_template, Blueprint, redirect, url_for, jsonify
 from .forms import SearchForm
 import json
+import requests
 
 main_blueprint = Blueprint('main', __name__,)
 
@@ -19,10 +20,24 @@ def search():
 
 @main_blueprint.route("/results")
 def search_results(search_query):
-    return render_template('main/results.html', results=None,
+    return render_template('main/results.html', results= query_API(search_query),
                            query=search_query)
 
 
 def query_API(searchterm):
-    tenor_apikey = None
-    return tenor_apikey
+    lmt = 10
+    tenor_apikey = '4NVGKX55MSMJ'
+    tenor_anon = '0361d5061be248679b8be21891021709'
+    r = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s&anon_id=%s" % (searchterm, tenor_apikey, lmt, tenor_anon))
+    if r.status_code == 200:
+        top_8gifs = json.loads(r.content)
+    else:
+        top_8gifs = None
+    top = top_8gifs["results"][0]["media"][0]["tinygif"]["url"]
+    abc = len(top_8gifs['results'])
+    imglist = []
+
+    for i in range(abc):
+        imglist.append(top_8gifs["results"][i]["media"][0]["tinygif"]["url"])
+
+    return imglist
